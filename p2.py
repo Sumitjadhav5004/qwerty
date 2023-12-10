c24ff254bdc64e1032d98d6f6f87edb65eb129bd
Sumitjadhav5004 <117936512+Sumitjadhav5004@users.noreply.github.com>
2023-12-10 17:48:24 +0530

    Create p2.py
    qwert

---
+++ p2.py
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void reverseSent(string s) {
+    stack<string> st;
+    for (int i = 0; i < s.length(); i++) {
+        string word = "";
+        while (s[i] != ' ' && i < s.length()) {
+            word += s[i];
+            i++;
+        }
+        st.push(word);
+    }
+
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+}
+
+bool isPalindrome(string s) {
+    int left = 0, right = s.length() - 1;
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main() {
+    string s = "hwe wedc erv errd";
+    
+    // Reverse the sentence
+    cout << "Reversed Sentence: ";
+    reverseSent(s);
+
+    // Check if the sentence is a palindrome
+    if (isPalindrome(s)) {
+        cout << "The sentence is a palindrome." << endl;
+    } else {
+        cout << "The sentence is not a palindrome." << endl;
+    }
+
+    return 0;
+}
